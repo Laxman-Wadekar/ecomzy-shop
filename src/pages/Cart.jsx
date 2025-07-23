403548d3483e4217596f445c6a547dc68ab0cbf6
Laxman Wadekar <laxmanvwadekar@gmail.com>
2025-07-23 15:28:44 +0530

    cart edited

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -58,7 +58,7 @@ const Cart = () => {
 					<h1 className="text-gray-700 font-semibold text-xl mb-2">
 						Your cart is empty!
 					</h1>
-					<NavLink to={"/"}>
+					<NavLink to={"/ecomzy-shop"}>
 						<button className="bg-green-600 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-semibold hover:text-green-700 p-3 px-10 tracking-wider">
 							Shop Now
 						</button>
